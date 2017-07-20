1aab113574c7b559b428b16eb26bcca82848ded0
github username <github email address>
2017-07-20 10:42:35 -0400

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,16 @@
 def unsafe?(speed)
-
+	if speed > 60 || speed < 40
+		true
+	elsif speed > 40 && speed < 60
+		false
+	end
 end
 
 
 
 def not_safe?(speed)
-	
+	#conditional ? action_if_true : action_if_false
+	#age < 2 ? "baby" : "not a baby"
+	speed > 60 || speed < 40 ? true : false
+	speed > 40 && speed < 60 ? false : true 
 end
-	
-
-
